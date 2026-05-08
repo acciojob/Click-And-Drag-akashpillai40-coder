1fa456371b95fad76abe8fd464a47968529cdd8d
Akash <akashpillai40@gmail.com>
2026-05-08 07:54:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-
+<script>
 const slider = document.querySelector(".items");
 
-let isDown = false;
-let startX;
-let scrollLeft;
+let isDragging = false;
+let startX = 0;
+let startScroll = 0;
 
 slider.addEventListener("mousedown", (e) => {
-  isDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-});
-
-slider.addEventListener("mouseup", () => {
-  isDown = false;
+  isDragging = true;
+  startX = e.pageX;
+  startScroll = slider.scrollLeft;
 });
 
 slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
+  if (!isDragging) return;
 
-  e.preventDefault();
-
-  const x = e.pageX - slider.offsetLeft;
-  const walk = x - startX;
-
-  slider.scrollLeft = scrollLeft - walk;
+  const walk = e.pageX - startX;
+  slider.scrollLeft = startScroll - walk;
 });
+
+slider.addEventListener("mouseup", () => {
+  isDragging = false;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDragging = false;
+});
+</script>
